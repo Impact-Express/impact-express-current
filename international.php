29455c2dfd9f74b851021004a2fafc2629b64c39
Richard Bailey <richard@impactexpress.co.uk>
2019-01-14 13:37:56 +0000

    getting smaller

--- international.php
+++ international.php
@@ -8,20 +8,21 @@ $theParent = wp_get_post_parent_id(get_the_ID());
 
 //while(have_posts()) {
 the_post(); ?>
-dadsadasdas
+
     <main id="main-content">
         <section id="services" class="services">
-            <div class="container" style="text-align: left;">
-
-                <div class="country-image">
+            <div class="international-banner">
+                <div class="container international-banner-image">
                     <img src="<?= get_field('hero_image')['url']; ?>" alt="<?= get_field('hero_image')['alt']; ?>">
+                    <?php get_template_part('quick-quote'); ?>
                 </div>
+            </div>
+            <div class="container" style="text-align: left;">    
                 <!-- Breadcrumbs -->
                 <?php if ($theParent): ?>
                     <div class="container" style="text-align: left;">
                         You are here:
                         <a href="/">Home</a> >
-                        <!--suppress XmlUnboundNsPrefix -->
                         <ul class="breadcrumbs">
                             <li>
                                 <a href='/services'>Sevices</a> >
@@ -29,7 +30,6 @@ dadsadasdas
                             <li>
                                 <a href='<?= get_permalink($theParent); ?>'><?= get_the_title($theParent); ?></a> >
                             </li>
-                            <!--suppress XmlUnboundNsPrefix -->
                             <li>
                                 <?php the_title(); ?>
                             </li>
@@ -50,7 +50,6 @@ dadsadasdas
                         <br>
                     <?php endif;?>
                 </div>
-                <?php get_template_part('quick-quote'); ?>
                 <div id="country-bottom-text">
                     <?php if (get_field('bottom_text')): ?>
                         <br>
