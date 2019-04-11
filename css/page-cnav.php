1d36c2f2f687667bd2527b5fb6e00af2786eafcd
Richard Bailey <richard@impactexpress.co.uk>
2019-04-11 14:15:06 +0000

    Added google tags to cnav template

--- css/page-cnav.php
+++ css/page-cnav.php
@@ -6,32 +6,13 @@
 <!DOCTYPE html>
 <html>
 <head>
-<!-- Google Tag Manager -->
 
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-
-})(window,document,'script','dataLayer','GTM-PB9CB6J');</script>
-
-<!-- End Google Tag Manager -->
 <?php
 	wp_head();
 	the_post();
 ?>
 </head>
 <body>
-<!-- Google Tag Manager (noscript) -->
-
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PB9CB6J"
-
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-
-<!-- End Google Tag Manager (noscript) -->
 <div id="ie-logo">
 	<a href="/">
 		<img src="<?= get_theme_file_uri('images/navbar-logo.png'); ?>" alt="Impact Express Wholesale Ltd Logo">
@@ -65,5 +46,21 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 	wp_footer();
 ?>
 </div>
+
+<script>
+    (function (i, s, o, g, r, a, m) {
+        i['GoogleAnalyticsObject'] = r;
+        i[r] = i[r] || function () {
+            (i[r].q = i[r].q || []).push(arguments)
+        }, i[r].l = 1 * new Date();
+        a = s.createElement(o),
+            m = s.getElementsByTagName(o)[0];
+        a.async = 1;
+        a.src = g;
+        m.parentNode.insertBefore(a, m)
+    })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+    ga('create', 'UA-88886441-1', 'impactexpress.co.uk');
+    ga('send', 'pageview');
+</script>
 </body>
 </html>
